836b6d4b6f93584167116ffb74b04fd37dc09ae7
ji23hye <Jihye.Park-2@student.uts.edu.au>
2025-05-12 17:57:10 +1000

    Add Bear_Jihye prefab

---
+++ Assets/bearAttack_Jihye.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class bearAttack_Jihye : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("isPlayerNear", true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("isPlayerNear", false);
+        }
+    }
+}
